ce47ddf4d4460c275dddc49e7cb4395ebbad28b5
alathea23 <135459921+alathea23@users.noreply.github.com>
2023-11-18 21:53:39 -0800

    starter code plus database construction

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+  otehrKEy: 'tag_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+  otherKey: 'product_id'
+})
 
 module.exports = {
   Product,
